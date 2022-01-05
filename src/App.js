baeaa6e5d321dd7dd46aa74be689f883a0f87386
ebruucgun <ucgunebru@gmail.com>
2022-01-05 12:41:42 +0300

    package.json deploy environment configured

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
   return (
     
     <div>
-      <Router>
+      <Router basename="/contacts">
         <TopBar/>
           <Switch>
             <Route exact path="/" component={HomePage} />
