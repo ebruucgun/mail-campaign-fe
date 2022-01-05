ad9f7871d4137f02217dcae1f704d1bd6c4e9910
ebruucgun <ucgunebru@gmail.com>
2022-01-05 20:23:02 +0300

    homepage updated

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -6,7 +6,7 @@ class TopBar extends React.Component{
   render(){
     return (
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-        <a className="navbar-brand" href="#">Campaign</a>
+        <Link className="navbar-brand" href="#">Campaign</Link>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
