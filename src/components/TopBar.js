d15b83b69df51411cbb3b6d9d5e245e2291be7ec
ebruucgun <ucgunebru@gmail.com>
2022-01-05 18:24:35 +0300

    Hash router added

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -6,6 +6,10 @@ class TopBar extends React.Component{
   render(){
     return (
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <a className="navbar-brand" href="#">Campaign</a>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
 
   <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
     <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
